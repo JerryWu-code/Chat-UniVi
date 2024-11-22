15c832232a345b65ccb7fba68aed79cea231cdc9
Jerry <953855668@qq.com>
2024-11-22 17:52:27 +0800

    Update stat.

--- zero_shot_stat.py
+++ zero_shot_stat.py
@@ -1,4 +1,5 @@
 import json
+import stat
 
 def load_jsonl(file_path):
     data = []
@@ -32,20 +33,34 @@ def analyze_data(data, save_to=None):
                 f.write(json.dumps(item) + '\n')
     return stat_data, single_data, singleno_data
 
+def stat_avg_duration(train_path, test_path, val_path):
+    vid_duration = {}
+    for path in [train_path, test_path, val_path]:
+        data = load_jsonl(path)
+        for item in data:
+            vid = item['vid']
+            if vid not in vid_duration:
+                vid_duration[vid] = item['duration']
+    avg_duration = sum(vid_duration.values()) / len(vid_duration)
+    return avg_duration
+
 if __name__ == '__main__':
+    train_path = "/home/weiji/yqin/data/vmr/nextVMR/annos/train_v3.jsonl"
     test_path = "/home/weiji/yqin/data/vmr/nextVMR/annos/test_v3.jsonl"
     val_path = "/home/weiji/yqin/data/vmr/nextVMR/annos/val_v3.jsonl"
 
-    test_data = load_jsonl(test_path)
-    val_data = load_jsonl(val_path)
+    # test_data = load_jsonl(test_path)
+    # val_data = load_jsonl(val_path)
 
-    stat_val, single_val, singleno_val = analyze_data(val_data, save_to="single_val.jsonl")
-    stat_test, single_test, singleno_test = analyze_data(test_data, save_to="single_test.jsonl")
+    # stat_val, single_val, singleno_val = analyze_data(val_data, save_to="single_val.jsonl")
+    # stat_test, single_test, singleno_test = analyze_data(test_data, save_to="single_test.jsonl")
     
-    print(f"Val: {stat_val}")
-    print(f"Test: {stat_test}")
+    # print(f"Val: {stat_val}")
+    # print(f"Test: {stat_test}")
     
     """
     Val: {'empty': 2340, 'multi': 1007, 'single': 8461}
     Test: {'empty': 6448, 'multi': 2769, 'single': 24577}
     """
+    avg_duration = stat_avg_duration(train_path, test_path, val_path)
+    print(f"Avg duration: {avg_duration:.2f}")
